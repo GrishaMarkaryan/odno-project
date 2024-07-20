164eb2ba77af1ce5d9341e5e9a00c39703de0719
GrishaMarkaryan <grisha-markaryan@mail.ru>
2024-07-21 00:11:53 +0300

    edited the whole project

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -7,42 +7,41 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 
 export const CreateForm = () => {
-    const [user] = useAuthState(auth);
-    const navigate = useNavigate();
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
-    interface CreateFormData {
-        title: string;
-        description: string;
-    }
+  interface CreateFormData {
 
-    const schema = yup.object().shape({
-        title: yup.string().required('Mi anun greq stex'),
-        description: yup.string().required('Mi ban greq stex'),
-      })
-    
-    const { register, handleSubmit, formState: {errors} } = useForm <CreateFormData>({
-        resolver: yupResolver(schema)
-    })  
+    description: string;
+  }
 
-    const postsRef = collection(db, 'cucak'); // т.е. создали ссылку к коллекции cucak
-    
-    const onCreatePost = async (data: CreateFormData) => { // передаем в базу данных информацию 
-        await addDoc(postsRef, {      // в коллекцию cucak передаем документ из 4-х элементов (title, descr-n, userName, userId)
-          title: data.title,
-          description: data.description, // можно вместо этих 2 строк написать ...data,
-          userName: user?.displayName,
-          userId: user?.uid,
-        }) 
-        navigate('/');
-    }
+  const schema = yup.object().shape({
 
-    return (
-      <form  onSubmit={handleSubmit(onCreatePost)} >
-        <input placeholder="Title.." {...register('title')} />
-        <p style={{color: "red"}}> {errors.title?.message} </p>
-        <textarea placeholder="Description.." {...register('description')} />
-        <p style={{color: "red"}}> {errors.description?.message} </p>
-        <input type="submit" />
-      </form>
-    )
+    description: yup.string().required('Please provide the Description'),
+  })
+
+  const { register, handleSubmit, formState: { errors } } = useForm<CreateFormData>({
+    resolver: yupResolver(schema)
+  })
+
+  const postsRef = collection(db, 'cucak'); // т.е. создали ссылку к коллекции cucak
+
+  const onCreatePost = async (data: CreateFormData) => { // передаем в базу данных информацию 
+    await addDoc(postsRef, {      // в коллекцию cucak передаем документ из 4-х элементов (title, descr-n, userName, userId)
+      description: data.description, // можно вместо этих 2 строк написать ...data,
+      userName: user?.displayName,
+      userId: user?.uid,
+    })
+    navigate('/');
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onCreatePost)} >
+      <div> New post </div>
+
+      <textarea placeholder="Description.." {...register('description')} className="inputField" />
+      <p > {errors.description?.message} </p>
+      <input type="submit" value={'Create post'} />
+    </form>
+  )
 }
--- src/pages/main/somePost.tsx
+++ src/pages/main/somePost.tsx
@@ -3,6 +3,7 @@ import { Post } from './main'
 import { auth, db } from '../../config/firebase';
 import { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import './somePost.css';
 
 interface Props {
     postFromFirestore: Post;
@@ -14,9 +15,9 @@ interface Like {
 }
 
 export const SomePost = (props: Props) => {
-    const {postFromFirestore} = props;
+    const { postFromFirestore } = props;
     const [user] = useAuthState(auth);
-    
+
     const [likes, setLikes] = useState<Like[] | null>(null) // likes является массивом, в котором будут объекты
 
     const likesRef = collection(db, 'likes');
@@ -24,55 +25,58 @@ export const SomePost = (props: Props) => {
 
     const getLikes = async () => {
         const data = await getDocs(likesDoc);
-        setLikes(data.docs.map((doc) => ({userId: doc.data().userId, likeId: doc.id })))  // length нашего возвращаемого массива равен количеству лайков на посте
+        setLikes(data.docs.map((doc) => ({ userId: doc.data().userId, likeId: doc.id })))  // length нашего возвращаемого массива равен количеству лайков на посте
     }
 
-    const hasUserLiked = likes?.find((like) => like.userId === user?.uid) 
+    const hasUserLiked = likes?.find((like) => like.userId === user?.uid)
 
     useEffect(() => {
         getLikes()
     }, [])
 
     const addLike = async () => {
-      try{
-        const newDoc = await addDoc(likesRef, {userId: user?.uid, postId: postFromFirestore.id});
-        if (user) {     // используем здесь optimistic rendering, чтобы наш палец автоматически поднимался / опускался
-            setLikes((prev) => prev? [...prev, {userId: user.uid, likeId: newDoc.id}] : [ {userId: user.uid, likeId: newDoc.id} ])
-        }}
-      catch(err) {console.log('sxal ka')}
+        try {
+            const newDoc = await addDoc(likesRef, { userId: user?.uid, postId: postFromFirestore.id });
+            if (user) {     // используем здесь optimistic rendering, чтобы наш палец автоматически поднимался / опускался
+                setLikes((prev) => prev ? [...prev, { userId: user.uid, likeId: newDoc.id }] : [{ userId: user.uid, likeId: newDoc.id }])
+            }
+        }
+        catch (err) { console.log('sxal ka') }
     }
 
     const removeLike = async () => {
-        try{
-          const likeToDeleteQuery = query( // выбираем лайк, который хотим убрать
-            likesRef, 
-            where('postId', '==', postFromFirestore.id),
-            where('userId', '==', user?.uid)
+        try {
+            const likeToDeleteQuery = query( // выбираем лайк, который хотим убрать
+                likesRef,
+                where('postId', '==', postFromFirestore.id),
+                where('userId', '==', user?.uid)
             )
-          const likeToDeleteData = await getDocs(likeToDeleteQuery); // получаем data этого лайка
-          const likeId = likeToDeleteData.docs[0].id;  // получаем id из data
-          const likeToDelete = doc(db, 'likes', likeId)  // созадем референс
-          await deleteDoc(likeToDelete); // удаляем лайк наконец
+            const likeToDeleteData = await getDocs(likeToDeleteQuery); // получаем data этого лайка
+            const likeId = likeToDeleteData.docs[0].id;  // получаем id из data
+            const likeToDelete = doc(db, 'likes', likeId)  // созадем референс
+            await deleteDoc(likeToDelete); // удаляем лайк наконец
 
-          if (user) {     
-              setLikes((prev) => prev && prev.filter((like) => like.likeId !== likeId)) // с помощью фильтра убираем отсюда лайки, которые мы удалили сверху
-          }}
-        catch(err) {console.log('sxal ka')}
-      }
+            if (user) {
+                setLikes((prev) => prev && prev.filter((like) => like.likeId !== likeId)) // с помощью фильтра убираем отсюда лайки, которые мы удалили сверху
+            }
+        }
+        catch (err) { console.log('sxal ka') }
+    }
 
-    return ( 
-    <div>
-        <div className="title">
-            <h2> {postFromFirestore.title}</h2>
+    return (
+        <div className="post-container">
+            <p className='author'> @{postFromFirestore.userName}</p>
+
+            <div className="post-content">
+                <div> {postFromFirestore.description} </div>
+            </div>
+            <div className="footer">
+                <button onClick={hasUserLiked ? removeLike : addLike}> {hasUserLiked ? <> &#128078; </> : <>&#128077;</>} </button>
+
+                <div className="likes">
+                    <p>{likes ? likes.length : 0} Likes</p>
+                </div>
+            </div>
         </div>
-        <div className="body">
-            <div> {postFromFirestore.description} </div>
-        </div>
-        <div className="footer">
-            <p> @{postFromFirestore.userName}</p>
-            <button onClick={hasUserLiked? removeLike : addLike}> { hasUserLiked? <> &#128078; </> : <>&#128077;</>} </button>
-            {likes && <p> Likes {likes?.length} </p>}
-        </div>
-    </div>
     )
 }
